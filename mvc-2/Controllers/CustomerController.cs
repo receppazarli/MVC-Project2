3442681b948a7fb7ad78e7868ef6650f1ab537c9
Recep Pazarlı <receppazarli99@hotmail.com>
2024-09-15 11:25:03 +0300

    Add project files.

---
+++ mvc-2/Controllers/CustomerController.cs
@@ -0,0 +1,62 @@
+﻿using mvc_2.Data;
+using mvc_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_2.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View(CustomerData.Customers);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customerData)
+        {
+            CustomerData.Customers.Add(customerData);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Update(int id)
+        {
+            var customer = CustomerData.Customers.
+                Where(x => x.Id == id).FirstOrDefault();
+            return View(customer);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Customer newCustomer)
+        {
+            Customer editCustomer = CustomerData.Customers.
+                Where(x => x.Id == newCustomer.Id).FirstOrDefault();
+
+            editCustomer.FirstName = newCustomer.FirstName;
+            editCustomer.LastName = newCustomer.LastName;
+            editCustomer.Address = newCustomer.Address;
+            editCustomer.City = newCustomer.City;
+            editCustomer.County = newCustomer.County;
+            editCustomer.NationalityId = newCustomer.NationalityId;
+            editCustomer.Phone = newCustomer.Phone;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(Customer newcustomer)
+        {
+            var customer = CustomerData.Customers.
+                Where(x => x.Id == newcustomer.Id).FirstOrDefault();
+            CustomerData.Customers.Remove(customer);
+            return View(customer);
+        }
+    }
+}
---
+++ mvc-2/Data/CustomerData.cs
@@ -0,0 +1,38 @@
+﻿using mvc_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc_2.Data
+{
+    public class CustomerData
+    {
+        public static List<Customer> Customers = new List<Customer>() {
+
+            new Customer
+            {
+                Id = 1,
+                FirstName = "Recep",
+                LastName = "Pazarlı",
+                NationalityId = "11111111111",
+                City = "Bursa",
+                County = "İnegöl",
+                Address = "Merkez mahallesi",
+                Phone = "0123 458 7474"
+            },
+            new Customer
+            {
+                Id = 2,
+                FirstName = "Ahmet",
+                LastName = "Yılmaz",
+                NationalityId = "22222222222",
+                City = "İstanbul",
+                County = "Kadıköy",
+                Address = "Merkez mahallesi",
+                Phone = "1234 567 1212"
+            }
+
+        };
+    }
+}
